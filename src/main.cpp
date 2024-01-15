894297d7bb94bd0e1533c713a77f86cf8e1434da
feroi35 <feroi@hotmail.fr>
2024-01-15 22:35:40 +0100

    factorisation code

--- src/main.cpp
+++ src/main.cpp
@@ -2,71 +2,52 @@
 #include "parser.h"
 #include "static_solve.h"
 #include "dualized_formulation.h"
+#include <cstdlib>
 
 // #include <string>
 
 
 int main(int argc, char **argv) {
 
-    if (argc < 4) {
-        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <filename>" << "method" << "(verbose)" << std::endl;
         exit(1);
     }
     char* filename = argv[1];
-    unsigned int time_limit = 60;
     char* method = argv[2];
-
-    // char* verb = argv[3];
-    // int verbose = std::stoi(verb);
-
-    int verbose = *argv[3] - '0';
-
-
+    int verbose = 0;
+    if (argc > 3) {
+        verbose = atoi(argv[3]);
+    }
+    unsigned int time_limit = 60;
     unsigned int time_limit_robust_obj = 60;
     unsigned int time_limit_robust_cstr = 60;
 
+    IloEnv env;
+    Instance instance(env, filename);
+    // instance.display();
 
     if(strcmp(method, "static") == 0) {
-        IloEnv env;
-        Instance instance(env, filename);
-        // instance.display();
         static_solve(env, instance, time_limit, verbose);
-
-        if(verbose >= 1){
-            double obj; 
-            obj = instance.compute_static_score(verbose);
-            std::cout << "obj static = " << obj << std::endl;
-            double robust_associated_obj;
-            robust_associated_obj = instance.compute_robust_score(env, instance.sol, time_limit_robust_obj, verbose);
-            std::cout << "obj robust = " << robust_associated_obj << std::endl;
-            double robust_associated_cstr;
-            robust_associated_cstr = instance.compute_robust_constraint(env, instance.sol, time_limit_robust_cstr, verbose);
-            std::cout << "cstr robust = " << robust_associated_cstr << " with S = " << instance.S << std::endl;
-        }
-
-        env.end(); 
-    }
-
-    if(strcmp(method, "dualized") == 0){
-        IloEnv env;
-        Instance instance(env, filename);
-        // instance.display();
+    } else if (strcmp(method, "dualized") == 0){
         dualized_solve(env, instance, time_limit, verbose);
-
-        if(verbose >= 1){
-            double obj; 
-            obj = instance.compute_static_score(verbose);
-            std::cout << "obj static = " << obj << std::endl;
-            double robust_associated_obj;
-            robust_associated_obj = instance.compute_robust_score(env, instance.sol, time_limit_robust_obj, verbose);
-            std::cout << "obj robust = " << robust_associated_obj << std::endl;
-            double robust_associated_cstr;
-            robust_associated_cstr = instance.compute_robust_constraint(env, instance.sol, time_limit_robust_cstr, verbose);
-            std::cout << "cstr robust = " << robust_associated_cstr << " with S = " << instance.S << std::endl;
-        }
-
-        env.end(); 
+    } else {
+        std::cout << "Method not recognized" << method << std::endl;
+        std::cout << "Method should be either 'static' or 'dualized'" << std::endl;
     }
 
+    if(verbose > 0){
+        double obj; 
+        obj = instance.compute_static_score(verbose);
+        std::cout << "obj static = " << obj << std::endl;
+        double robust_associated_obj;
+        robust_associated_obj = instance.compute_robust_score(env, instance.sol, time_limit_robust_obj, verbose);
+        std::cout << "obj robust = " << robust_associated_obj << std::endl;
+        double robust_associated_cstr;
+        robust_associated_cstr = instance.compute_robust_constraint(env, instance.sol, time_limit_robust_cstr, verbose);
+        std::cout << "cstr robust = " << robust_associated_cstr << " with S = " << instance.S << std::endl;
+    }
+
+    env.end(); 
     return 0;
 }
